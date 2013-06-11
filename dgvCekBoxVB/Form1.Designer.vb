79cbb42362fd6b5489a24974f70d16ec9b565cbc
Gege Satya Putra <bolakita.mania@gmail.com>
2013-06-11 20:04:51 +0700

    -

---
+++ dgvCekBoxVB/Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Column1 = New System.Windows.Forms.DataGridViewCheckBoxColumn
+        Me.ProductID = New System.Windows.Forms.DataGridViewTextBoxColumn
+        Me.ProductName = New System.Windows.Forms.DataGridViewTextBoxColumn
+        Me.ProductID1 = New System.Windows.Forms.DataGridViewTextBoxColumn
+        Me.ProductName1 = New System.Windows.Forms.DataGridViewTextBoxColumn
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.ProductID, Me.ProductName})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(425, 302)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ProductID1, Me.ProductName1})
+        Me.DataGridView2.Location = New System.Drawing.Point(443, 12)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(489, 302)
+        Me.DataGridView2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(362, 320)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Pilih"
+        Me.Column1.Name = "Column1"
+        '
+        'ProductID
+        '
+        Me.ProductID.DataPropertyName = "ProductID"
+        Me.ProductID.HeaderText = "ProducID"
+        Me.ProductID.Name = "ProductID"
+        '
+        'ProductName
+        '
+        Me.ProductName.DataPropertyName = "ProductName"
+        Me.ProductName.HeaderText = "Product Name"
+        Me.ProductName.Name = "ProductName"
+        '
+        'ProductID1
+        '
+        Me.ProductID1.HeaderText = "ProductID"
+        Me.ProductID1.Name = "ProductID1"
+        '
+        'ProductName1
+        '
+        Me.ProductName1.HeaderText = "Product Name"
+        Me.ProductName1.Name = "ProductName1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(944, 407)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents DataGridView2 As System.Windows.Forms.DataGridView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Column1 As System.Windows.Forms.DataGridViewCheckBoxColumn
+    Friend WithEvents ProductID As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents ProductName As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents ProductID1 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents ProductName1 As System.Windows.Forms.DataGridViewTextBoxColumn
+
+End Class
---
+++ dgvCekBoxVB/Form1.vb
@@ -0,0 +1,50 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+    Dim cnstring As String = "Data source = localhost;uid = sa;pwd = biofree;initial catalog = northwind"
+    Dim dt As New DataTable
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Using cn As New SqlConnection(cnstring)
+            cn.Open()
+            Dim cmd As New SqlCommand("select * from products", cn)
+
+            Dim reader As SqlDataReader = cmd.ExecuteReader
+            dt.Load(reader)
+
+            DataGridView1.AutoGenerateColumns = False
+            DataGridView1.DataSource = dt
+        End Using
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim _productID As New List(Of Integer)
+        For Each r As DataGridViewRow In DataGridView1.Rows
+            If r.Cells(0).Value = True Then
+                _productID.Add(r.Cells(1).Value)
+
+                Dim cell() As Object = {r.Cells(1).Value, r.Cells(2).Value}
+                DataGridView2.Rows.Add(cell)
+            End If
+        Next
+
+        Dim _s As String = ""
+
+        For Each i As Integer In _productID
+            _s &= i & ","
+        Next
+
+        Dim sql As String = "SELECT * FROM Products WHERE ProductID NOT IN (" & _s.TrimEnd(",") & ")"
+
+        Using cn As New SqlConnection(cnstring)
+            cn.Open()
+            Dim cmd As New SqlCommand(sql, cn)
+
+            Dim reader As SqlDataReader = cmd.ExecuteReader
+            dt = New DataTable
+            dt.Load(reader)
+
+            DataGridView1.DataSource = dt
+        End Using
+    End Sub
+End Class
